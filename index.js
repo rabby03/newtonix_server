8f0c91925a23c93cfebcf063678aff58058f003e
Mohammad F. Rabby <115248974+rabby03@users.noreply.github.com>
2023-04-12 16:46:07 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import env from 'dotenv'
-import {Configuration, OpenAIApi} from 'openai'
-
-const app = express()
-
-env.config()
-
-app.use(cors())
-app.use(bodyParser.json())
-
-
-// Configure open api
-const configuration = new Configuration({
-    organization: "PASTE YOUR ORGANIZATION ID HERE",
-    apiKey: process.env.API_KEY // VISIT .env AND MAKE CHANGES
-})
-const openai = new OpenAIApi(configuration)
-
-
-// listeninng
-app.listen("3080", ()=>console.log("listening on port 3080"))
-
-
-// dummy route to test
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
-
-
-//post route for making requests
-app.post('/', async (req, res)=>{
-    const {message} = req.body
-
-    try{
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${message}`,
-            max_tokens: 100,
-            temperature: .5
-        })
-        res.json({message: response.data.choices[0].text})
-
-    }catch(e){
-        console.log(e)
-        res.send(e).status(400)
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { errorHandler } from './middlewares.js';
+import { getChatGPTAPI } from './chatgpt.js';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+const api = await getChatGPTAPI();
+app.use(cors());
+app.use(express.json());
+app.use(errorHandler);
+app.get('/', (req, res) => {
+    res.send('MathGPT server is running OK');
+});
+app.post('/chatgpt/messages', async (req, res, next) => {
+    const { text, parentMessageId } = req.body;
+    try {
+        const response = await api.sendMessage(text, {
+            parentMessageId,
+        });
+        res.json({
+            answer: response.text,
+            messageId: response.id,
+        });
     }
-})
+    catch (error) {
+        if (error instanceof Error) {
+            return next(error);
+        }
+        const errorMessage = typeof error === 'string' ? error : 'Something went wrong';
+        return next(new Error(errorMessage));
+    }
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
